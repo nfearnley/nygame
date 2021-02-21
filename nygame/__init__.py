8103579e655b864d4eb53983ec4050ff5ba25915
Natalie Fearnley <nfearnley@gmail.com>
2021-02-21 05:34:05 -0500

    moved perf for nygame

--- nygame/__init__.py
+++ nygame/__init__.py
@@ -1,5 +1,6 @@
 from . import time, music, digifont
 from .game import Game
 from .digifont import DigiText
+from .perf import perf
 
-__all__ = ["time", "music", "digifont", "Game", "DigiText"]
+__all__ = ["time", "music", "digifont", "Game", "DigiText", "perf"]
