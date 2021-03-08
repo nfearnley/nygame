7b1865e66acb1411fa5848a84966856fdb644f9a
Natalie Fearnley <nfearnley@gmail.com>
2021-03-08 00:50:29 -0500

    fixed music export

--- nygame/__init__.py
+++ nygame/__init__.py
@@ -1,6 +1,7 @@
-from . import time, music, digifont, emoji
+from . import time, digifont, emoji
+from .music import music
 from .game import Game
 from .digifont import DigiText
 from .perf import perf
 
-__all__ = ["time", "music", "digifont", "emoji", "Game", "DigiText", "perf"]
+__all__ = ["time", "digifont", "emoji", "music", "Game", "DigiText", "perf"]
