6b7d4b9837ac8a2af2fe9240ca0482df65789f8e
Natalie Fearnley <nfearnley@gmail.com>
2021-03-07 23:32:31 -0500

    added emoji module

--- nygame/__init__.py
+++ nygame/__init__.py
@@ -1,6 +1,6 @@
-from . import time, music, digifont
+from . import time, music, digifont, emoji
 from .game import Game
 from .digifont import DigiText
 from .perf import perf
 
-__all__ = ["time", "music", "digifont", "Game", "DigiText", "perf"]
+__all__ = ["time", "music", "digifont", "emoji", "Game", "DigiText", "perf"]
