c6c5f70a6e266f85143d3b34c1d7c1778d1e9c68
Natalie Fearnley <nfearnley@gmail.com>
2021-03-10 01:59:36 -0500

    Added display scale

--- nygame/game.py
+++ nygame/game.py
@@ -4,17 +4,45 @@ from .music import music
 
 
 class Game:
-    def __init__(self, *, size=(800, 600), fps=30, bgcolor="black", showfps=False):
+    def __init__(self, *, size=(800, 600), scale=1, fps=30, bgcolor="black", showfps=False):
         pygame.init()
         self.clock = time.Clock()
-        self.surface = pygame.display.set_mode(size, pygame.DOUBLEBUF)
-        self.size = self.surface.get_size()
+        self._currsize = None
+        self._currscale = None
+        self.size = size
+        self.scale = scale
+        self.reset_display()
         self.running = True
         self.fps = fps
         self.showfps = showfps
         self.fps_font = font_cache.get_font("Consolas", 24)
         self.bgcolor = bgcolor
         music.init()
+
+    @property
+    def scale(self):
+        return self._scale
+
+    @scale.setter
+    def scale(self, newscale):
+        if newscale is None:
+            newscale = 1
+        if not isinstance(newscale, int) or newscale < 1:
+            raise ValueError(f"Invalid scale: {newscale}")
+        self._scale = newscale
+
+    def reset_display(self):
+        if (self._currscale, self._currsize) == (self.scale, self.size):
+            return
+        (self._currscale, self._currsize) = (self.scale, self.size)
+        if self.scale == 1:
+            self.out_surface = None
+            self.surface = pygame.display.set_mode(self.size, pygame.DOUBLEBUF)
+        else:
+            w, h = self.size
+            scaled_size = w * self.scale, h * self.scale
+            self.out_surface = pygame.display.set_mode(scaled_size, pygame.DOUBLEBUF)
+            self.surface = pygame.Surface(self.size)
 
     def run(self):
         while self.running:
@@ -24,11 +52,13 @@ class Game:
             for e in events:
                 if e.type == pygame.QUIT:
                     self.running = False
-                elif e.type == self.MUSIC_END_EVENT:
+                elif e.type == music.MUSIC_END_EVENT:
                     music.on_end(e)
             self.loop(events)
             if self.showfps:
                 self.draw_fps(self.clock.get_fps())
+            if self.out_surface is not None:
+                pygame.transform.scale(self.surface, self.out_surface.get_size(), self.out_surface)
             pygame.display.flip()
             self.clock.tick_busy_loop(self.fps)
 
