6802cfb19916014488a80a9331d3ce074815a65e
Natalie Fearnley <nfearnley@gmail.com>
2021-03-21 23:29:33 -0400

    updated imports

--- nygame/__init__.py
+++ nygame/__init__.py
@@ -1,8 +1,6 @@
-import os
-os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
-del os
+from . import _quietload
 
-from pygame import Rect, Color
+from pygame import Rect, Color, joystick
 from . import time, digifont, emoji, data
 from .music import music
 from .game import Game
@@ -10,4 +8,6 @@ from .digifont import DigiText
 from .perf import perf
 from .constants import *
 
-__all__ = ["Rect", "Color", "time", "digifont", "emoji", "data", "music", "Game", "DigiText", "perf"]
+del _quietload
+
+__all__ = ["Rect", "Color", "joystick", "time", "digifont", "emoji", "data", "music", "Game", "DigiText", "perf"]
